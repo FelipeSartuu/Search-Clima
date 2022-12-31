0ed3d9f77b32aafa12dffc21de4b8b299ff5a7f4
Felipe Sartorato <felipesartoratoalfano@gmail.com>
2022-12-30 22:36:20 -0300

    Corrigindo Design
    Fazendo com que o painel só apareça quando todas as informações estiverem concluídas

--- assets/script/script.js
+++ assets/script/script.js
@@ -32,7 +32,7 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
 function showInfo(json) {
     showWarning("")
 
-    document.querySelector(".resultado").style.display = "block"
+    
 
     document.querySelector(".titulo").innerHTML = `${json.name},  ${json.country}`
     document.querySelector(".tempInfo").innerHTML = `${json.temp} <sup>°C</sup>`
@@ -41,6 +41,8 @@ function showInfo(json) {
     document.querySelector(".temp img").setAttribute("src", `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
 
     document.querySelector(".ventoPonto").style.transform  = `rotate(${json.windAngle -90}deg)`
+
+    document.querySelector(".resultado").style.display = "block"
 }
 
 
